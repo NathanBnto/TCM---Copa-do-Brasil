c3947263463e95521116d32dce171db05ca103d1
NathanBnto <nathanbento963@gmail.com>
2024-11-05 23:36:21 -0300

    Add files via upload

--- sript_da_copa.js
+++ sript_da_copa.js
@@ -1,47 +1,121 @@
 function mostratimes(){
  var t = document.getElementById("mtimes");
- if(t.style.display=="none"){
-     t.style.display="block";
- } else {
-     t.style.display="block"
- }
+ 
+ t.style.display="block";
+ 
+}
+
+function fechamenu(){
+    
+    menuzin.classList.toggle('expandir');
+    main.style.filter = "brightness(100%)";
+    sera = false;
+}
+function mostratimesmob(){
+    var mj = document.getElementById("menujogos");
+    var mt = document.getElementById("menutimes");
+    var mc = document.getElementById("menucamps");
+    
+    if(mt.style.display == "none"){
+        mc.style.display = "none";
+        mj.style.display = "none";
+        mt.style.display = "block";  
+    } else {
+        mt.style.display = "none";
+    }
+    
 }
 function mostrajogos(){
     var j = document.getElementById("mjogos");
-    var tj = document.getElementById("textojogo")
-    if(j.style.display=="none"){
-        j.style.display="block";
-        j.style.marginTop="12px"
-        tj.style.marginTop ="13px"
+    var tj = document.getElementById("textojogo");
+   
+    j.style.display = "block";
+    j.style.marginTop = "12px";
+    tj.style.marginTop = "13px";
+}
+function mostrajogosmob(){
+    var mc = document.getElementById("menucamps");
+    var mt = document.getElementById("menutimes");
+    var mj = document.getElementById("menujogos");
+    if(mj.style.display == "none"){
+        mc.style.display = "none"
+        mt.style.display = "none"
+        mj.style.display = "block";  
     } else {
-        j.style.display="block"
-        j.style.marginTop="12px"
-        tj.style.marginTop ="13px"
+        mj.style.display = "none";
     }
+    
 }
 function mostracamps(){
     var c = document.getElementById("mcamps");
-    var tc = document.getElementById("textocamps")
-    if(c.style.display=="none"){
-        c.style.display="block";
-        c.style.marginTop="12px";
-        tc.style.marginTop ="13px";
+    var tc = document.getElementById("textocamps");
+    
+    c.style.display="block";
+    c.style.marginTop="12px";
+    tc.style.marginTop ="13px";
+}
+function mostracampsmob(){
+    var mc = document.getElementById("menucamps");
+    var mt = document.getElementById("menutimes");
+    var mj = document.getElementById("menujogos");
+
+    if(mc.style.display == "none"){
+        mt.style.display = "none";
+        mj.style.display = "none";
+        mc.style.display = "block";
     } else {
-        c.style.display="block";
-        c.style.marginTop="12px";
-        tc.style.marginTop ="13px";
+        mc.style.display = "none";
+    }
+}
+var sera = false;
+var menuzin = document.querySelector('.mobile');
+var btnExp = document.querySelector('#btn-exp');
+var main = document.getElementById('normal');
+var logo = document.getElementById('logo');
+btnExp.addEventListener('click',function(){
+    menuzin.classList.toggle('expandir');
+   
+    if(sera){
+        main.style.filter = "brightness(20%)";
+    } else {
+        main.style.filter = "brightness(100%)";     
+    }
+})
+
+function menumobile(){
+    var menuitens = document.getElementsByClassName('mobcoisas');
+    var icons = document.getElementsByClassName('timesicon');
+    
+    if(!sera){
+        
+        for(let i = 0; i<menuitens.length; i++){
+        menuitens[i].style.display = "flex";
+        menuitens[i].style.flex = "1";
+        menuitens[i].style.padding = "10px";
+        
+        }
+        for(let i = 0; i<icons.length; i++){
+            icons[i].style.paddingLeft = "5px";
+            }
+        sera = true;
+    } else {
+        for(let i = 0; i<menuitens; i++){
+            menuitens[i].style.display = "none";
+        }
+       
+        sera = false;
     }
 }
 
 function somecoisa(){
- var mt = document.getElementById("mtimes");
- var mj = document.getElementById("mjogos");
- var mc = document.getElementById("mcamps");
+ var t = document.getElementById("mtimes");
+ var j = document.getElementById("mjogos");
+ var c = document.getElementById("mcamps");
  var tj = document.getElementById("textojogo");
  var tc = document.getElementById("textocamps");
  tj.style.marginTop="0px";
  tc.style.marginTop="0px";
- mt.style.display="none";
- mj.style.display="none";
- mc.style.display="none";
+ t.style.display="none";
+ j.style.display="none";
+ c.style.display="none";
 }
